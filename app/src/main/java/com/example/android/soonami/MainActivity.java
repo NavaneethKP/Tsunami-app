4619a91793091d7fa908a2f5ffe196bd84698b0f
KP Navaneeth <kpnavaneeth12@gmail.com>
2017-06-21 22:48:01 -0200

    added condition for url and response code

--- app/src/main/java/com/example/android/soonami/MainActivity.java
+++ app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.soonami;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -45,8 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
     /** URL to query the USGS dataset for earthquake information */
     private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2012-12-01&minmagnitude=6";
-
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-12-01&minmagnitude=7";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -157,13 +157,22 @@ public class MainActivity extends AppCompatActivity {
             HttpURLConnection urlConnection = null;
             InputStream inputStream = null;
             try {
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.setReadTimeout(10000 /* milliseconds */);
-                urlConnection.setConnectTimeout(15000 /* milliseconds */);
-                urlConnection.connect();
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
+                //To establish a connection only if the url is not null
+                if(url!=null) {
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.setReadTimeout(10000 /* milliseconds */);
+                    urlConnection.setConnectTimeout(15000 /* milliseconds */);
+                    urlConnection.connect();
+                    inputStream = urlConnection.getInputStream();
+
+                    //Checking for the successful response code 200
+                    if(urlConnection.getResponseCode()==200) {
+                        jsonResponse = readFromStream(inputStream);
+                        return jsonResponse;
+                    }
+
+                }
             } catch (IOException e) {
                 // TODO: Handle the exception
             } finally {
@@ -175,7 +184,8 @@ public class MainActivity extends AppCompatActivity {
                     inputStream.close();
                 }
             }
-            return jsonResponse;
+            return null;
+
         }
 
         /**
@@ -201,6 +211,10 @@ public class MainActivity extends AppCompatActivity {
          * about the first earthquake from the input earthquakeJSON string.
          */
         private Event extractFeatureFromJson(String earthquakeJSON) {
+
+            //To check if the JSON response is null or length==0
+            if(TextUtils.isEmpty(earthquakeJSON))
+                return null;
             try {
                 JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
                 JSONArray featureArray = baseJsonResponse.getJSONArray("features");
